e9e3541ec9830f053abf79b4af562f8fb2badbbd
alexnicolescu <nicolescualexandru12@gmail.com>
2020-02-25 10:05:39 +0200

    Added buttons to the toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import {MatCardModule} from '@angular/material/card';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import { TopToolbarComponent } from './top-toolbar/top-toolbar.component';
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,7 +22,8 @@ import { TopToolbarComponent } from './top-toolbar/top-toolbar.component';
     MatInputModule,
     MatCardModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
