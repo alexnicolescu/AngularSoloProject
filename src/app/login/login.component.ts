09aa449d0f6ff9452503e580aca414c7cc6696cb
alexnicolescu <nicolescualexandru12@gmail.com>
2020-02-25 18:22:17 +0200

    Error message for login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'cp-login',
@@ -6,8 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  @Input()
+  signInError: string;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
