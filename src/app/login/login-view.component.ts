a6a31a5b3828209844a61f9d26e75d9546128e55
alexnicolescu <nicolescualexandru12@gmail.com>
2020-02-25 18:47:54 +0200

    Login View Component

---
+++ src/app/login/login-view.component.ts
@@ -0,0 +1,16 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'cp-login-view',
+  templateUrl: './login-view.component.html',
+  styleUrls: ['./login-view.component.css']
+})
+export class LoginViewComponent implements OnInit {
+  @Input()
+  signInError: string;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
