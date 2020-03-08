4c0c7121ce74f617f1b0e0569ba45e6ad5684eed
alexnicolescu <nicolescualexandru12@gmail.com>
2020-03-08 14:34:05 +0200

    Users-View components added

---
+++ src/app/users/users-view/users-list.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
+import {User} from '../user';
+
+@Component({
+  selector: 'cp-users-list',
+  templateUrl: './users-list.component.html',
+  styleUrls: ['./users-list.component.css']
+})
+export class UsersListComponent implements OnInit {
+
+  @Input()
+  users: Observable<User[]>;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/users/users-view/users-view.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {Observable} from 'rxjs';
+import {User} from '../user';
+import {UserService} from '../user.service';
+
+@Component({
+  selector: 'cp-users-view',
+  templateUrl: './users-view.component.html',
+})
+export class UsersViewComponent implements OnInit {
+  users: Observable<User[]>;
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.users = this.userService.getUsers();
+  }
+
+}
